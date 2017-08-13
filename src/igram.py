0256d6aacf55bbab709489e0fbe999111daafe61
Amal Karunarathna <nasashinega@gmail.com>
2017-08-13 17:00:40 +0530

    Add multiple file downloads

---
+++ src/igram.py
@@ -0,0 +1,26 @@
+import re
+import requests
+import sys
+from lxml import html
+
+# todo: Try threading for downloads
+
+
+class Download:
+    def __init__(self, url):
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        self.link = tree.xpath('//meta[@property="og:image"]/@content')[0]
+        print(self.link)
+        self.fetch()
+
+    def fetch(self):
+        r = requests.get(self.link, stream=True)
+        if r.status_code == 200:
+            with open(str(re.sub(r'^(\w|\W)*/', '', self.link)), 'wb') as f:
+                for chunk in r.iter_content(1024):
+                    f.write(chunk)
+
+# Iterate over input arguments
+for index in range(len(sys.argv) - 1):
+    Download(sys.argv[index + 1])
