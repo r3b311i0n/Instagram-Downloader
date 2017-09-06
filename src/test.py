ca83aaaf630caadb12c67d3b6f60976ff8982ce1
r3b311i0n <nasashinega@gmail.com>
2017-09-07 04:56:02 +0530

    Fix unit tests for slideshows :wrench: :construction_worker: :construction:

--- src/test.py
+++ src/test.py
@@ -7,23 +7,47 @@ class DownloadTest(unittest.TestCase):
         super().setUp()
 
     def test_download(self):
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia').fetch(), None)
+        try:
+            instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia')
+        except (IndexError, KeyError) as e:
+            self.fail(e)
 
     def test_multiple_images(self):
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia').fetch(), None)
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BXlVqs0gYJJ/?taken-by=sofiablackdelia').fetch(), None)
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BWsd0uoDmQu/?taken-by=kendalljenner').fetch(), None)
+        try:
+            instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia')
+            instagram.Download('https://www.instagram.com/p/BXlVqs0gYJJ/?taken-by=sofiablackdelia')
+            instagram.Download('https://www.instagram.com/p/BWsd0uoDmQu/?taken-by=kendalljenner')
+        except (IndexError, KeyError) as e:
+            self.fail(e)
 
     def test_multiple_videos(self):
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BYOjMOhgegp/?taken-by=sofiablackdelia').fetch(), None)
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BX8mD4AAyQI/?taken-by=sofiablackdelia').fetch(), None)
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BXqVx3qjUwd/?taken-by=kendalljenner').fetch(), None)
+        try:
+            instagram.Download('https://www.instagram.com/p/BYOjMOhgegp/?taken-by=sofiablackdelia')
+            instagram.Download('https://www.instagram.com/p/BX8mD4AAyQI/?taken-by=sofiablackdelia')
+            instagram.Download('https://www.instagram.com/p/BXqVx3qjUwd/?taken-by=kendalljenner')
+        except (IndexError, KeyError) as e:
+            self.fail(e)
 
     def test_mix_of_both(self):
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BUeZiJyjXtS/?taken-by=kendalljenner').fetch(), None)
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BRbzdH0DqTk/?taken-by=kendalljenner').fetch(), None)
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BRWANhLj6ph/?taken-by=kendalljenner').fetch(), None)
-        self.assertEqual(instagram.Download('https://www.instagram.com/p/BYdvYl1DJZE/?taken-by=alexisren').fetch(), None)
+        try:
+            instagram.Download('https://www.instagram.com/p/BUeZiJyjXtS/?taken-by=kendalljenner')
+            instagram.Download('https://www.instagram.com/p/BRbzdH0DqTk/?taken-by=kendalljenner')
+            instagram.Download('https://www.instagram.com/p/BRWANhLj6ph/?taken-by=kendalljenner')
+            instagram.Download('https://www.instagram.com/p/BYdvYl1DJZE/?taken-by=alexisren')
+        except (IndexError, KeyError) as e:
+            self.fail(e)
+
+    def test_image_slideshow(self):
+        try:
+            instagram.Download('https://www.instagram.com/p/BX1MmNIgkUj/?taken-by=sofiablackdelia')
+        except (IndexError, KeyError) as e:
+            self.fail(e)
+
+    def test_mix_slideshow(self):
+        try:
+            instagram.Download('https://www.instagram.com/p/BXq_rYZjAD5/?taken-by=alexisren')
+        except (IndexError, KeyError) as e:
+            self.fail(e)
 
     def tearDown(self):
         super().tearDown()
