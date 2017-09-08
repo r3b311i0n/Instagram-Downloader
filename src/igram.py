216f180d9418bd1ebddf4a6958b924acfac3e7e7
r3b311i0n <nasashinega@gmail.com>
2017-09-08 05:31:11 +0530

    Implement threaded downloads

--- src/igram.py
+++ src/igram.py
@@ -3,19 +3,20 @@ import json
 import re
 import requests
 import sys
+import threading
 
 
-# todo: Try threading for downloads
-
-
-class Download:
+class Download(threading.Thread):
     def __init__(self, url):
+        threading.Thread.__init__(self)
         page = requests.get(url)
-        tree = html.fromstring(page.content)
-        shared_data = json.loads(tree.xpath('//script/text()')[1][21:-1])
+        self.tree = html.fromstring(page.content)
+        self.shared_data = json.loads(self.tree.xpath('//script/text()')[1][21:-1])
+
+    def run(self):
         try:
             media_array = \
-                shared_data['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children'][
+                self.shared_data['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children'][
                     'edges']
 
             for resource_link in media_array:
@@ -29,9 +30,9 @@ class Download:
 
         except KeyError:
             try:
-                link = tree.xpath('//meta[@property="og:video"]/@content')[0]
+                link = self.tree.xpath('//meta[@property="og:video"]/@content')[0]
             except IndexError:
-                link = tree.xpath('//meta[@property="og:image"]/@content')[0]
+                link = self.tree.xpath('//meta[@property="og:image"]/@content')[0]
             finally:
                 # noinspection PyUnboundLocalVariable
                 print(link)
@@ -48,4 +49,4 @@ class Download:
 
 # Iterate over input arguments
 for index in range(len(sys.argv) - 1):
-    Download(sys.argv[index + 1])
+    Download(sys.argv[index + 1]).start()
--- src/test.py
+++ src/test.py
@@ -8,44 +8,44 @@ class DownloadTest(unittest.TestCase):
 
     def test_download(self):
         try:
-            instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia')
+            instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia').start()
         except (IndexError, KeyError) as e:
             self.fail(e)
 
     def test_multiple_images(self):
         try:
-            instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia')
-            instagram.Download('https://www.instagram.com/p/BXlVqs0gYJJ/?taken-by=sofiablackdelia')
-            instagram.Download('https://www.instagram.com/p/BWsd0uoDmQu/?taken-by=kendalljenner')
+            instagram.Download('https://www.instagram.com/p/BTvAszmgJxb/?taken-by=sofiablackdelia').start()
+            instagram.Download('https://www.instagram.com/p/BXlVqs0gYJJ/?taken-by=sofiablackdelia').start()
+            instagram.Download('https://www.instagram.com/p/BWsd0uoDmQu/?taken-by=kendalljenner').start()
         except (IndexError, KeyError) as e:
             self.fail(e)
 
     def test_multiple_videos(self):
         try:
-            instagram.Download('https://www.instagram.com/p/BYOjMOhgegp/?taken-by=sofiablackdelia')
-            instagram.Download('https://www.instagram.com/p/BX8mD4AAyQI/?taken-by=sofiablackdelia')
-            instagram.Download('https://www.instagram.com/p/BXqVx3qjUwd/?taken-by=kendalljenner')
+            instagram.Download('https://www.instagram.com/p/BYOjMOhgegp/?taken-by=sofiablackdelia').start()
+            instagram.Download('https://www.instagram.com/p/BX8mD4AAyQI/?taken-by=sofiablackdelia').start()
+            instagram.Download('https://www.instagram.com/p/BXqVx3qjUwd/?taken-by=kendalljenner').start()
         except (IndexError, KeyError) as e:
             self.fail(e)
 
     def test_mix_of_both(self):
         try:
-            instagram.Download('https://www.instagram.com/p/BUeZiJyjXtS/?taken-by=kendalljenner')
-            instagram.Download('https://www.instagram.com/p/BRbzdH0DqTk/?taken-by=kendalljenner')
-            instagram.Download('https://www.instagram.com/p/BRWANhLj6ph/?taken-by=kendalljenner')
-            instagram.Download('https://www.instagram.com/p/BYdvYl1DJZE/?taken-by=alexisren')
+            instagram.Download('https://www.instagram.com/p/BUeZiJyjXtS/?taken-by=kendalljenner').start()
+            instagram.Download('https://www.instagram.com/p/BRbzdH0DqTk/?taken-by=kendalljenner').start()
+            instagram.Download('https://www.instagram.com/p/BRWANhLj6ph/?taken-by=kendalljenner').start()
+            instagram.Download('https://www.instagram.com/p/BYdvYl1DJZE/?taken-by=alexisren').start()
         except (IndexError, KeyError) as e:
             self.fail(e)
 
     def test_image_slideshow(self):
         try:
-            instagram.Download('https://www.instagram.com/p/BX1MmNIgkUj/?taken-by=sofiablackdelia')
+            instagram.Download('https://www.instagram.com/p/BX1MmNIgkUj/?taken-by=sofiablackdelia').start()
         except (IndexError, KeyError) as e:
             self.fail(e)
 
     def test_mix_slideshow(self):
         try:
-            instagram.Download('https://www.instagram.com/p/BXq_rYZjAD5/?taken-by=alexisren')
+            instagram.Download('https://www.instagram.com/p/BXq_rYZjAD5/?taken-by=alexisren').start()
         except (IndexError, KeyError) as e:
             self.fail(e)
 
